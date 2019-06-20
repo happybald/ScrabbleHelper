637a2aee3d111a4d32355e2c448c84aa779a5f28
happyBald <dens123456@gmail.com>
2019-06-20 14:19:05 +0300

    Добавление музыки, сжатие повторяющихся слов в сортированом списке по баллам, переделан помощник

--- Source.cpp
+++ Source.cpp
@@ -5,6 +5,8 @@
 #include <Windows.h>
 #include <conio.h>
 #include <ctime>
+#include <mmsystem.h>
+#pragma comment(lib, "winmm.lib") 
 using namespace std;
 
 HANDLE hStdOut;
@@ -21,9 +23,11 @@ HWND hwnd;
 #define TABLE_SIZE 9
 struct Node {
 	char word[30];
-	char info[800];
+	char *info = new char[800];
+	char *info1 = NULL;
+	char *info2 = NULL;
+	char *info3 = NULL;
 	int points;
-	int sovp = 0;
 	Node *next;
 };
 
@@ -52,6 +56,52 @@ void SetColor(int text, int background) {
 }
 WORD colorConvertTo(int ForgC, int BackC){
 	return ((BackC & 0x0F) << 4) + (ForgC & 0x0F);
+}
+void removeDuplicates(Node* head)
+{
+	/* Pointer to traverse the linked list */
+	Node* current = head;
+
+	/* Pointer to store the next pointer of a node to be deleted*/
+	Node* next_next;
+
+	/* do nothing if the list is empty */
+	if (current == NULL) {
+		return;
+	}
+
+	/* Traverse the list till last node */
+	while (current->next != NULL)
+	{
+		/* Compare current node with next node */
+		if (strcmp(current->word, current->next->word) == 0)
+		{
+			/* The sequence of steps is important*/
+			next_next = current->next->next;
+			if (next_next->info1 == NULL) {
+				next_next->info1 = new char[800];
+				strcpy(next_next->info1, current->info);
+			}
+			else {
+				if (next_next->info2 == NULL) {
+					next_next->info2 = new char[800];
+					strcpy(next_next->info2, current->info);
+				}
+				else {
+					if (next_next->info3 == NULL) {
+						next_next->info3 = new char[800];
+						strcpy(next_next->info3, current->info);
+					}
+				}
+			}
+			delete current->next;
+			current->next = next_next;
+		}
+		else /* This is tricky: only advance if no deletion */
+		{
+			current = current->next;
+		}
+	}
 }
 struct Node* swap(struct Node* ptr1, struct Node* ptr2)
 {
@@ -117,7 +167,10 @@ void bubbleSort(struct Node** head, int count)
 	}
 }
 char *Entering(char **table) {
-	char *enterword = new char[30];
+	int size;
+	cout << "Enter size of the word :";
+	cin >> size;
+	char *enterword = new char[size];
 	cout << "Enter word with * : ";
 	drawTable(table, 0);
 	cin >> enterword;
@@ -125,15 +178,14 @@ char *Entering(char **table) {
 	int wordsize = strlen(enterword);
 	char *word = new char[wordsize];
 	strcpy(word, enterword);
-	delete enterword;
 	for (int i = 0; i < strlen(word); i++) {
 		word[i] = toupper(word[i]);
 	}
 	return word;
 }
-int pointscount(char*word) {
+int pointscount(char*word,int size) {
 	int i = 0;
-	for (int c = 0; c < strlen(word); c++) {
+	for (int c = 0; c < size; c++) {
 		switch (word[c])
 		{
 		case 'E':
@@ -198,7 +250,10 @@ void append(struct Node** head_ref, char *new_data, char *info)
 	}
 	strcpy(new_node->word, new_data);
 	strcpy(new_node->info, info);
-	new_node->points = pointscount(new_data);
+	new_node->info1 = NULL;
+	new_node->info2 = NULL;
+	new_node->info3 = NULL;
+	new_node->points = pointscount(new_data, strlen(new_data));
 	new_node->next = NULL;
 	if (*head_ref == NULL)
 	{
@@ -226,34 +281,58 @@ void printList(Node *node)
 		SetColor(LightGreen, Black);
 		cout << "Description" << endl;
 		cout << " " << node->info << endl;
+		if (node->info1 != NULL) {
+			cout << " " << node->info1 << endl;
+		}
+		if (node->info2 != NULL) {
+			cout << " " << node->info2 << endl;
+		}
+		if (node->info3 != NULL) {
+			cout << " " << node->info3 << endl;
+		}
 		SetColor(White, Black);
+		Sleep(80);
 		node = node->next;
 	}
 }
-int bestV(Node *node, char **table) {
-	if (node == NULL) {
-		return 0;
-	}
-	char *str = new char[TABLE_SIZE];
-	int c = 0;
-	for (int i = 0; i < TABLE_SIZE; i++) {
-		for (int j = 0; j < TABLE_SIZE; j++) {
-			if (table[i][j] != ' ') {
-				str[c] = table[i][j];
-				c++;
+char *removeDuplicates(char *arr, int &size) {
+
+	int end = size;
+
+	for (int i = 0; i < end; i++) {
+		for (int j = i + 1; j < end; j++) {
+			if (arr[i] == arr[j]) {
+				int shiftLeft = j;
+				for (int k = j + 1; k < end; k++, shiftLeft++) {
+					arr[shiftLeft] = arr[k];
+				}
+				end--;
+				j--;
 			}
 		}
 	}
+	
+	size = end;
+	char *whitelist = new char[end];
+	for (int i = 0; i < end; i++) {
+		whitelist[i] = arr[i];
+	}
+	return whitelist;
+}
+/*int bestV(Node *node, char **table, char*str,int size) {
+	if (node == NULL) {
+		return 0;
+	}
 	int sovp = 0;
 	for (int j = 1; j < strlen(node->word)-1; j++) {
-		for (int i = 0; i < c+1; i++) {
+		for (int i = 0; i < size; i++) {
 			if (node->word[j] == str[i]) {
 				j++;
 				sovp++;
 			}
 		}
 	}
-	if (sovp >= (strlen(node->word) - 2)) {
+	if (sovp >= (strlen(node->word))/2) {
 		node->sovp = 100;
 	}
 	else {
@@ -263,10 +342,10 @@ int bestV(Node *node, char **table) {
 		return sovp;
 	}
 }
-void find_max(struct Node*temp, char**letters) {
+void find_max(struct Node*temp, char**table) {
 
 	if (temp != NULL) {
-		int max_value = bestV(temp, letters);
+		int max_value = bestV(temp, table);
 		while (temp != NULL) {
 			if (temp->sovp >= max_value) {
 				for (int i = 0; i < 20; i++) {
@@ -295,7 +374,7 @@ void find_max(struct Node*temp, char**letters) {
 			temp = temp->next;
 		}
 	}
-}
+}*/
 void gotoxy(int x, int y)
 {
 	COORD Gon = { x, y };
@@ -458,7 +537,7 @@ bool check(char *word, int size) {
 	}
 	return 0;
 }
-bool SelectWordOnTable(char**table) {
+bool SelectWordOnTable(char**table,int&res) {
 	bool tab[TABLE_SIZE][TABLE_SIZE] = { 0 };
 	HBRUSH def = CreateSolidBrush(RGB(255, 255, 255));
 	HBRUSH selectR = CreateSolidBrush(RGB(0, 200, 0));
@@ -652,67 +731,79 @@ bool SelectWordOnTable(char**table) {
 	}
 	if (check(selectedword, pos + 1) == 1) {
 		cout << "	Nice true word!" << endl;
+		res += pointscount(selectedword, pos + 1);
 		drawTable(table, 0);
 		return 1;
 	}
 	cout << endl;
 	return 0;
 }
-void helper(Node *&fword, int &fsize, char *&word, char**table) {
+void helper(Node *&fword, int &fsize, char *&word,int &wordsize,char**table) {
 	fword = NULL;
 	fsize = 0;
-	word = Entering(table);
-	int wanted = 0;
-	for (int i = 0; i < strlen(word); i++) {
-		if (word[i] == '*') {
-			wanted++;
+	word = new char[TABLE_SIZE];
+	int c = 0;
+	for (int i = 0; i < TABLE_SIZE; i++) {
+		for (int j = 0; j < TABLE_SIZE; j++) {
+			if (table[i][j] != ' ') {
+				word[c] = table[i][j];
+				c++;
+			}
 		}
 	}
-	char *file = new char[17];
-	strcpy(file, "Dictionary/");
-	strncat(file, word, 1);
-	strcat(file, ".txt");
-	char *temp = new char[30];
-	char *info = new char[800];
-	ifstream f;
-	f.open(file);
-	if (!f.is_open()) {
-		cout << "can't find" << endl;
-		system("pause");
+	word = removeDuplicates(word, c);
+	for (int i = 0; i < c; i++) {
+		cout << word[i] << " ";
 	}
-	else {
-		while (!f.eof()) {
-			f >> temp;
-			bool ind = false;
-			int count = 0;
-			if (strlen(word) == strlen(temp)) {
-				for (int i = 0; i < strlen(temp); i++) {
-					temp[i] = toupper(temp[i]);
-					if (word[i] == temp[i]) {
-						count++;
+	wordsize = c;
+	cout << endl;
+	for (int letters = 1; letters<=TABLE_SIZE; letters++) {
+		for (int strpos = 0; strpos < c; strpos++) {
+			char *file = new char[17];
+			strcpy(file, "Dictionary/");
+			strncat(file, word + strpos, 1);
+			strcat(file, ".txt");
+			char *temp = new char[30];
+			char *info = new char[800];
+			ifstream f;
+			f.open(file);
+			if (f.is_open()){
+				while (!f.eof()) {
+					f >> temp;
+					int count = 0;
+					bool ind = false;
+					if (letters == strlen(temp) && temp[0] == word[strpos]) {
+						ind = true;
+						for (int i = 0; i < c; i++) {
+							for (int j = 0; j < letters; j++) {
+								temp[j] = toupper(temp[j]);
+								if (temp[j] == word[i]) {
+									count++;
+								}
+							}
+						}
+					}
+					if (ind == true && count >= letters) {
+						f.getline(info, 800, '\n');
+						append(&fword, temp, info);
+						fsize++;
+					}
+					else {
+						f.ignore(600, '\n');
 					}
 				}
-				if (word[strlen(word) - 1] == '*' || word[strlen(word) - 1] == temp[strlen(temp) - 1]) {
-					ind = true;
-				}
-				if ((count >= (strlen(temp) - wanted)) && ind == true) {
-					f.getline(info, 800, '\n');
-					append(&fword, temp, info);
-					fsize++;
-				}
-			}
-			else {
-				f.ignore(600, '\n');
+				f.close();
+				bubbleSort(&fword, fsize);
 			}
 		}
-		f.close();
-		bubbleSort(&fword, fsize);
 	}
 }
 
 
 int main(int argc, char *argv[]) {
 	SetConsoleTitle("Scrabble Helper");
+	Sleep(60);
+	//PlaySound("fon.wav", NULL, SND_FILENAME || SND_LOOP);
 	hdc = GetDC(GetConsoleWindow());
 	hwnd = GetConsoleWindow();
 	SetColor(White, Black);
@@ -721,46 +812,69 @@ int main(int argc, char *argv[]) {
 	for (int i = 0; i < TABLE_SIZE; i++) {
 		table[i] = new char[TABLE_SIZE];
 	}
-
+	bool setf = 0;
+	cout << "Set first word auto?(1-yes,0-no)" << endl;
+	cin >> setf;
 	initTable(table, TABLE_SIZE);
-	for (int i = 0; i < TABLE_SIZE; i++) {
-		for (int j = 0; j < TABLE_SIZE; j++) {
-			cout << table[i][j] << " ";
+	if (setf == 0) {
+		cout << "Enter word from " << TABLE_SIZE << " letters :" << endl;
+		for (int i = 0; i < TABLE_SIZE; i++) {
+			cin >> table[TABLE_SIZE / 2][i];
 		}
-		cout << endl;
 	}
+	for (int j = 0; j < TABLE_SIZE; j++) {
+		cout << table[TABLE_SIZE / 2][j] << " ";
+	}
+	cout << endl;
 	Node *fword = NULL;
 	char *word = new char[15];
+	int wordsize;
 	int fsize = 0;
 	int ind = 1;
+	int res = 0;
+	drawTable(table, 0);
 	while (ind!=0) {
 		SetColor(LightCyan, Black);
 		cout << endl;
-		cout << " 1 - Enter new Word" << endl;
-		cout << " 2 - Show best variant" << endl;
-		cout << " 3 - Show all variants" << endl;
-		cout << " 4 - Drop on table" << endl;
+		cout << " 1 - Show points for letters" << endl;
+		cout << " 2 - Enter new Word" << endl;
+		cout << " 3 - Show best variant" << endl;
+		cout << " 4 - Show all variants" << endl;
+		cout << " 5 - Drop on table" << endl;
 		SetColor(White, Black);
 		cin >> ind;
 		switch (ind) {
 		case 1: {
 			system("cls");
-			delete fword;
-			helper(fword, fsize, word, table);
+			SetColor(Yellow, Black);
+			cout << endl << endl;
+			cout << "\tPoints for every letter : " << endl << endl;
+			SetColor(LightMagenta, Black);
+			cout << "\t1 - E, A, I, O, N, R, T, L, S, U" << endl << endl;
+			cout << "\t2 - D, G" << endl << endl;
+			cout << "\t3 - B, C, M, P" << endl << endl;
+			cout << "\t4 - F, H, V, W, Y" << endl << endl;
+			cout << "\t5 - K" << endl << endl;
+			cout << "\t8 - J, X" << endl << endl;
+			cout << "\t10 - Q, Z" << endl << endl;
+			drawTable(table, 0);
 			break;
 		}
 		case 2: {
 			system("cls");
-			cout << endl << "Entered word is : ";
-			for (int i = 0; i < strlen(word); i++) {
-				cout << *(word+i) << " ";
-			}
-			cout << endl;
-			find_max(fword, table);
+			delete fword;
+			helper(fword, fsize, word, wordsize, table);
+			removeDuplicates(fword);
+			printList(fword);
 			drawTable(table, 0);
 			break;
 		}
 		case 3: {
+			system("cls");
+			drawTable(table, 0);
+			break;
+		}
+		case 4: {
 			system("cls");
 			cout << endl << "Entered word is : ";
 			for (int i = 0; i < strlen(word); i++) {
@@ -771,7 +885,7 @@ int main(int argc, char *argv[]) {
 			drawTable(table, 0);
 			break;
 		}
-		case 4: {
+		case 5: {
 			system("cls");
 			COORD Selected;
 			char a;
@@ -811,10 +925,11 @@ int main(int argc, char *argv[]) {
 			} while (ind == 0);
 			table[Selected.X][Selected.Y] = toupper(a);
 			drawTable(table, 0);
-			if (SelectWordOnTable(table) == 0) {
+			if (SelectWordOnTable(table,res) == 0) {
 				table[Selected.X][Selected.Y] = ' ';
 				cout << "Error! Cant find this word in dictionary" << endl;
 			}
+			cout << res << endl;
 			drawTable(table, 0);
 			break;
 		}
